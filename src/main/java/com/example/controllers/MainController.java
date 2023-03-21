bccc8a8056b547f8e66ffce2d518dd861836bb49
sarah <sxrahhp@gmail.com>
2023-03-21 13:47:28 +0100

    select construida con Thymeleaf

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.entities.Estudiante;
+import com.example.entities.Facultad;
 import com.example.services.EstudianteService;
+import com.example.services.FacultadService;
 
 @Controller
 @RequestMapping("/")
@@ -19,6 +21,12 @@ public class MainController {
 
     @Autowired
     private EstudianteService estudianteService; 
+
+    //Se acaba de añadir el servicio de Facultades, porque es necesario para dar de alta a un estudiante. 
+    //Inyectar el servicio de facultad, sino no hay acceso a los registros de la tabla facultad. 
+
+    @Autowired
+    private FacultadService facultadService; 
 
 //Aquí dentro se encuentran los handler mapping creo. 
 
@@ -38,12 +46,17 @@ public ModelAndView listar(){
 }
 
 /**
- * Método para mostrar el formulario de alta de estudiante 
+ * Método para mostrar el formulario de alta de estudiante. 
+ * 
+ * Se tiene que añadir un listado de facultades, para que se tome desde la base de datos. 
  */
 @GetMapping("/frmAltaEstudiante")
 public String formularioAltaEstudiante(Model model){
 
+   List<Facultad> facultades = facultadService.findaAll(); 
+   
     model.addAttribute("estudiante", new Estudiante()); 
+    model.addAttribute("facultades", facultades); 
 
     return "views/formularioAltaEstudiante"; 
 }
