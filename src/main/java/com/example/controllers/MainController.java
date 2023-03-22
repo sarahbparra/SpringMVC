d2f781c98d53d20b73079b049a20b3214e84a98c
sarah <sxrahhp@gmail.com>
2023-03-22 09:35:53 +0100

    Antes de insertar teléfonos

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -67,7 +68,10 @@ public String formularioAltaEstudiante(Model model){
  * Método que recibe los datos procedentes de los controles del formulario 
  */
 @PostMapping("/altaEstudiante")
-public String altaEstudiante(){
+//El método altaEstudiante() tiene que recibir un atributo del modelo. 
+public String altaEstudiante(@ModelAttribute Estudiante estudiante){
+
+    estudianteService.save(estudiante);
 
     return "redirect:/listar"; 
 
